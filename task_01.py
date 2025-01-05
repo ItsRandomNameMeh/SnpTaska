ad6cc060286a0c8623d44a9bfc34439385fbf2e5
ItsRandomNameMeh <d.enotow@yandex.ru>
2025-01-05 15:21:07 +0300

    Initial commit

---
+++ task_01.py
@@ -0,0 +1,17 @@
+import re #библиотека для работы с регулярными выражениями
+def is_palindrome(test_string):
+    if(type(test_string)!=str):
+        test_string = str(test_string)
+    test_string = test_string.replace(' ','')
+    test_string = test_string.lower()
+    test_string = re.sub(r'[^\w\s]',"",test_string)#регулярнео выражение удаляющее знаки преминания
+    if(test_string == test_string[::-1]):
+        return True
+    return False
+
+print(is_palindrome("A man, a plan, a canal -- Panama")) # => True
+print(is_palindrome("Madam, I'm Adam!")) # => True
+print(is_palindrome(333)) # => True
+print(is_palindrome(None)) # => False
+print(is_palindrome("Abracadabra")) # => False
+print(is_palindrome("AbbA")) # => True
